dfe3acd62e13474767fa059221374c4347be2a26
DanilodeRamos <116719740+DanilodRamos@users.noreply.github.com>
2026-07-06 19:29:25 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -15,6 +18,7 @@ import model.entities.Seller;
 public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
+
 //construtor com argumento
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
@@ -44,38 +48,36 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
-				+ "FROM seller INNER JOIN department  "
-					+"ON seller.DepartmentId = department.Id  "
-					+ "WHERE seller.Id = ?" );
-			//CONFIGURANDO ?
-			st.setInt(1 , id);
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department  "
+							+ "ON seller.DepartmentId = department.Id  " + "WHERE seller.Id = ?");
+			// CONFIGURANDO ?
+			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
-				//testar se o vendedor e null e metodo pra retorna o vendedor pra id
+				// testar se o vendedor e null e metodo pra retorna o vendedor pra id
 				Department dep = instantiateDepartment(rs);
 				Seller obj = instantiateSeller(rs, dep);
 				return obj;
-				
+
 			}
 			return null;
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
-			DB.closeResultSet(rs); // nao fecha conexao 
+			DB.closeResultSet(rs); // nao fecha conexao
 		}
 	}
-private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
-	Seller obj = new Seller();
-	obj.setId(rs.getInt("Id"));
-	obj.setName(rs.getString("Name"));
-	obj.setEmail(rs.getString("Email"));
-	obj.setBaseSalary(rs.getDouble("BaseSalary"));
-	obj.setBirthDate(rs.getDate("BirthDate"));
-	obj.setDepartment(dep);
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
 		return obj;
 	}
 
@@ -91,6 +93,49 @@ private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLExcepti
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+			// CONFIGURANDO ?
+			st.setInt(1, department.getId());
+
+			rs = st.executeQuery();
+			// declarando uma lista
+			List<Seller> list = new ArrayList<>();
+			// declarando chave valor map
+			Map<Integer, Department> map = new HashMap<>();
+
+			while (rs.next()) {
+
+				// antes de instaciar ve se existe
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				// incluindo teste
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				} // com esse esquema o map .get pega ele se nao existir volta null
+
+				// testar se o vendedor e null e metodo pra retorna o vendedor pra id
+
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs); // nao fecha conexao
+		}
 	}
 
 }
